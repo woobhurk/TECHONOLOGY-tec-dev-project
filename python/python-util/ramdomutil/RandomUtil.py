5e0236a9425d7d728b0d9f065aa1252eb9a29c73
tyfanchz <woobhurk@qq.com>
2021-01-13 17:48:50 +0800

    python\python-util\ramdomutil\RandomUtil.py python\python-util\ramdomutil\WeightRandom.py python\python-util\ramdomutil\__init__.py

---
+++ python/python-util/ramdomutil/RandomUtil.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+from random import Random
+from collections import OrderedDict
+from typing import *
+
+class RandomUtil():
+    """- 生成随机数。
+    """
+    random: Random = Random()
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class ModuleTest():
+    def __init__(self) -> None:
+        super().__init__()
+
+    def main(self):
+        pass
+
+
+if __name__ == "__main__":
+    ModuleTest().main()
