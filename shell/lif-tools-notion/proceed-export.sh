804b318d5f121ebd61e17f700972f48482738aac
tyfanchz <woobhurk@qq.com>
2021-06-08 17:36:01 +0800

    shell\lif-tools-notion\proceed-export.sh

--- shell/lif-tools-notion/proceed-export.sh
+++ shell/lif-tools-notion/proceed-export.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
 function renameDirAndFile() {
-    echo "--------------------------------"
-    echo "Removing MD5 suffix in all dir and files..."
+    echo --------------------------------
+    echo Removing MD5 suffix in all dir and files...
     DIR="$1"
     # Remove MD5 suffix in all directories and files.
     # 1. Find all paths in current path.
     # 2. Sort the paths in reverse order so the renaming will performs correctly.
     # 3. And rename all the paths to new ones in a `while` loop.
     find "$DIR" -iname "*" | sort -r | while read -r NAME; do
-        NEW_NAME="$(echo "$NAME" | sed -E -e "s/(.*) \w{32}(\..*)?/\1\2/")"
-        if [[ "$NAME" != "$NEW_NAME" ]]; then
+        PARENT_DIR="${NAME%/*}"
+        # Remove MD5 suffix in filenames.
+        FILE_NAME="$(echo "${NAME##*/}" | sed -E -e "s/(.*) \w{32}(\..*)?/\1\2/")"
+        NEW_NAME="$PARENT_DIR/$FILE_NAME"
+        #NEW_NAME="$(echo "$NAME" | sed -E -e "s/(.*) \w{32}(\..*)?/\1\2/")"
+        # Do not rename base dir
+        if [[ "$NAME" != "$DIR" && "$NAME" != "$NEW_NAME" ]]; then
             echo "RENAME: $NAME -> $NEW_NAME"
             mv "$NAME" "$NEW_NAME"
         fi
@@ -18,19 +23,48 @@ function renameDirAndFile() {
     echo
 }
 
-function removeTitle() {
-    echo "--------------------------------"
-    echo "Removing title of all files..."
+function proceedAllLines() {
+    echo --------------------------------
+    echo Proceeding lines of all files...
     DIR="$1"
-    # Remove title of all files.
-    find "$DIR" -iname "*" -type f | while read -r FILE; do
+    #find "$DIR" -iname "*" -type f | while read -r FILE; do
+    findAllTextFile "$DIR" | while read -r FILE; do
         echo "PROCEED: $FILE"
-        # Read next line, and replace line which starts with "#".
-        sed -Ei "N; /^#.*\n/d" "$FILE"
+        # 1. Remove title of all files.
+        #   1.1 Read next line,
+        #   1.2 Replace line which starts with "#".
+        # 2. Remove extra top blank lines.
+        # 3. Remove extra tailing spaces.
+        sed -Ei -e "N; /^#.*\n/d" \
+            -e "/./,\$!d" \
+            -e "s/[ \t]+$//" "$FILE"
         # Append new line to end of file.
         echo "" >> "$FILE"
     done
     echo
+}
+
+function addFilename() {
+    echo --------------------------------
+    echo Adding filename as title...
+    DIR="$1"
+    #find "$DIR" -iname "*" -type f | while read -r FILE; do
+    findAllTextFile "$DIR" | while read -r FILE; do
+        TITLE_LINE="${FILE##*/}"
+        TITLE_LINE="- ${TITLE_LINE%.*}"
+        FIRST_LINE="$(sed -n "1 p" "$FILE")"
+        if [[ "$FIRST_LINE" != "$TITLE_LINE" ]]; then
+            echo "ADD: $FILE"
+            sed -Ei "1 i $TITLE_LINE" "$FILE"
+        fi
+    done
+    echo
+}
+
+function findAllTextFile() {
+    DIR="$1"
+    FILE_PATTERN=".*\.\(md\|txt\)"
+    find "$DIR" -iregex "$FILE_PATTERN" -type f
 }
 
 function main() {
@@ -38,8 +72,12 @@ function main() {
     if [[ -z "$DIR" ]]; then
         read -r -p "Input dir: " DIR
     fi
+    echo "DIR = $DIR"
+    echo
+
     renameDirAndFile "$DIR"
-    removeTitle "$DIR"
+    proceedAllLines "$DIR"
+    addFilename "$DIR"
     echo ALL DONE!
 }
 
